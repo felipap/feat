c24aeecb1a65c1ccaff9ccfb53c38295c82c68d2
Felipe Pires <pires.a.felipe@gmail.com>
2019-05-30 20:11:37 -0700

    what

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # https://stackoverflow.com/questions/15746675
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='assembler',
     version='0.0.1',
     description='',
     license='allrightsreserved',
-    packages=['assembler'],
+    packages=find_packages(exclude=['tests*']),
     author='Felipe Pires',
     author_email='pires.a.felipe@gmail.com',
     keywords=['example'],
